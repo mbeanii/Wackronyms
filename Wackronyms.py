4a5798e48ccd4799ecff31d7ec23315e2a9296d9
mbeanii <marcus.bean@outlook.com>
2022-11-13 12:11:19 +0000

    Add barebones wsgi

--- Wackronyms.py
+++ Wackronyms.py
@@ -2,11 +2,12 @@ import player
 import randomLetter
 import random
 
+
 def play_game():
     player_list = player.get_players(3)
 
-    print(randomLetter.get_random_string(random.randint(3,6)))
+    print(randomLetter.get_random_string(random.randint(3, 6)))
+
 
 if __name__ == '__main__':
     play_game()
-
