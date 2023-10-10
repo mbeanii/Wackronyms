50cd4558f5dbe2abf602f6e4fe91b2e70946bb9b
mbeanii <marcus.bean@outlook.com>
2023-10-10 19:58:25 +0100

    websockets working; /host updating player_list dynamically

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 Wackronyms is a party-style word game for up 3-6 players.
 
 To run:
-1) (recommended) Activate the virtual environment venv
+1) Activate the virtual environment venv
 2) Build the package "pip install -e ." from wackronyms dir
 3) Run `python wsgi/app.py`
 4) Look for <host_url> - the last line of the flask command output
-5) Navigate to <host_url> from guest devices
+5) Navigate to <host_url>/host from the host device
+6) Navigate to <host_url> from guest devices
--- wsgi/app.py
+++ wsgi/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+from flask_socketio import SocketIO
 from wackronyms import Wackronyms
+import logging
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 wackronyms = Wackronyms()
-
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/lobby", methods=["GET", "POST"])
@@ -15,6 +18,7 @@ def lobby():
         while not name:
             return render_template("get_name_form.html", title="No Name Provided")
         wackronyms.add_player(name)
+        socketio.emit('update_list', {'player_list': wackronyms.get_player_names()}, namespace='/host')
         return render_template("welcome.html", title="Welcome", name=name)
 
     return render_template("get_name_form.html", title="Add player")
@@ -24,5 +28,10 @@ def host():
     global wackronyms
     return render_template("host.html", player_names=wackronyms.get_player_names())
 
+@socketio.on('connect', namespace='/host')
+def handle_connect():
+    logging.debug('WebSocket client connected')
+    socketio.emit('update_list', {'player_list': wackronyms.get_player_names()}, namespace='/host')
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True)
